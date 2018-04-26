28d53410509f067f689b261c035c03bc2cbd45b7
-=NeXuS=- <schwartzmann.a@gmail.com>
2018-04-26 10:04:27 +0200

    Create config.php

---
+++ pico_edit/config.php
@@ -0,0 +1,11 @@
+<?php
+
+global $backend_password;
+
+/*
+ * This should be a sha256 hash of your password.
+ * Use a tool like https://convertstring.com/Hash/SHA256 to generate.
+ */
+$backend_password = '11B0E5EC25B5214B4B773A9739847BBBFFF0A41156B9D8E33BF324D09536711E';
+
+?>
